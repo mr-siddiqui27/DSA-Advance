7531f48b27ab7be7ac9bcd99237905955355f9c6
Mojammil Husain <mhsiddiqui027@gmail.com>
2026-01-28 02:01:06 +0530

    maze problem

--- Recursion/src/StringPractice/Maze.java
+++ Recursion/src/StringPractice/Maze.java
@@ -1,8 +1,11 @@
 package StringPractice;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
-        System.out.println(pathCount(3,3));
+//        System.out.println(stepsPrint(3,3, ""));
+        System.out.println(stepReturn(3,3,""));
     }
 
 
@@ -14,4 +17,46 @@ public class Maze {
         int down = pathCount(r-1,c);
         return right+down;
     }
+
+    public static int stepsPrint(int r, int c, String s){
+
+        // When reach bottom then add all R
+        if(r==1){
+            for(int i=1; i<=(c-1); i++){
+                s +='R';
+            }
+            System.out.println(s);
+            return 1;
+        }
+        // when reach right end then add all D
+        if(c==1){
+            for(int i=1; i<=(r-1); i++){
+                s +='D';
+            }
+            System.out.println(s);
+            return 1;
+        }
+        int right = stepsPrint(r,c-1, s+'R');
+        int down = stepsPrint(r-1,c, s+'D');
+        return right+down;
+    }
+
+    // return steps
+    // same as stepPrint but in different way
+    public static ArrayList<String> stepReturn(int r, int c, String p){
+        if(r==1 && c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> right = new ArrayList<>();
+        ArrayList<String> down = new ArrayList<>();
+        if(c>1) right = stepReturn(r,c-1,p+'R');
+        if(r>1) down = stepReturn(r-1,c, p+'D');
+
+        right.addAll(down);
+        return right;
+    }
+
+
 }
